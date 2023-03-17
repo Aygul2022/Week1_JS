57ad58b3dc77e15ccccbf76b8c2305a5ad1f9fa0
Aygul2022 <119407989+Aygul2022@users.noreply.github.com>
2023-03-17 12:17:54 +0300

    first commit

---
+++ home_work/index.js
@@ -0,0 +1,42 @@
+let salary = 40000;
+let salary2 = 22000;
+console.log(salary + salary2);  //62000
+ 
+let word = "Владивосток";
+let num = 2000;
+console.log(word + " " + num);  //Владивосток 2000
+
+let timeMessenger = 128;
+let timeFoto = 254;
+let timeVC = 137;
+let timeStandUp = 201;
+console.log(timeMessenger + timeFoto + timeVC + timeStandUp);  //720
+
+let a = 2;
+let b = 5;
+console.log(a**b);  //32
+
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!");
+
+const greetings = "Привет";
+const name = "Кот";
+console.log(greetings + ", " + name); //Привет, Кот
+
+console.log(`${greetings}, ${name}`); //Привет, Кот
+
+
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(fahrenheit + " градуса по Фаренгейту — это " + celsius + " градуса по Цельсию.");
+console.log(`${fahrenheit} градуса по Фаренгейту — это ${celsius} градуса по Цельсию.`);
+
+let time;
+ time = 34;
+console.log(time);
+console.log(`'Старт поездки. Осталось минут: ${time}'`);
+time-=15;
+console.log(`'Немного сторис в соцсетях. Осталось минут:  ${time}'`);
+time-=10;
+console.log(`'Немного не новостей, но событий. Осталось минут: ${time}.'`);
+time = 0;
+console.log(`'Вы приехали. Добро пожаловать в Москву'`);
